6d2a218744eb82032e2eb999fef6f4844e49c2dd
Arnav-Gr0ver <arnavgrover@gmail.com>
2024-11-07 22:29:31 -0500

    update instruct
    Signed-off-by: Arnav-Gr0ver <arnavgrover@gmail.com>

--- task2_eval.py
+++ task2_eval.py
@@ -28,7 +28,7 @@ STOCK_TICKERS_HIGHEST_CAP_US = [
 ]
 
 eval_config = Task2Config(
-    model_name="meta-llama/Llama-3.2-1B-Instruct",
+    model_name="meta-llama/Llama-3.2-3B-Instruct",
     bnb_config=BitsAndBytesConfig(load_in_8bit=True),
     tickers=STOCK_TICKERS_HIGHEST_CAP_US,
     end_date=END_DATE,
